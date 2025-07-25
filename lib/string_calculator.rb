80e0f06ebd02333b15613310c98fa6d2f7b31d5c
Bhupendra Singh Negi <bhupendrasinghnegi@Bhupendra-MacBook.local>
2025-07-25 10:39:21 +0530

    String Calculator | haddle empty and nil

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,2 +1,8 @@
 class StringCalculator
+    def self.add(numbers)
+      return nil if numbers.nil?
+
+      return 0 if numbers.strip.empty?
+
+    end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,6 +3,15 @@ require 'string_calculator'
 RSpec.describe StringCalculator do
   describe '.add' do
     it 'returns 0 for an empty string' do
+      expect(StringCalculator.add("")).to eq(0)
+    end
+
+    it 'returns 0 for an empty string' do
+        expect(StringCalculator.add(" ")).to eq(0)
+    end
+
+    it 'returns nil for nil' do
+        expect(StringCalculator.add(nil)).to eq(nil)
     end
   end
 end
